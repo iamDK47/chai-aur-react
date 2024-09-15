69929f666d6f3f12d0e70b01f050ff7ebb4687eb
Daniyal <daniyalkamran33@gmail.com>
2024-09-16 02:04:10 +0500

    counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,10 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [counter, setCounter] = useState(15)
+
+  // let counter = 15
+  // let add = function(){
+  //   counter = counter + 1
+  //   console.log(counter)
+  //   document.getElementById('c').textContent = counter
+  // }
+  
+  const add = function(){
+    if (counter<20){
+      setCounter(counter + 1)
+    }else{
+      alert('no')
+    }
+  }
 
   return (
-   <h1>chai aur react</h1>
+    <>
+      <h1>count is : {counter}</h1>
+
+      <button onClick={add}>Increase {counter}</button>
+      <button>Decrease {counter}</button>
+    </>
   )
 }
 
