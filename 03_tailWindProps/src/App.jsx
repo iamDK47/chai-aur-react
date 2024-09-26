66ab20b32412b5333e42a49a2194531e23ae2fd3
Daniyal <daniyalkamran33@gmail.com>
2024-09-26 15:40:07 +0500

    props

--- 03_tailWindProps/src/App.jsx
+++ 03_tailWindProps/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,30 +10,9 @@ function App() {
   return (
     <>
       <h1 className = 'bg-green-400 text-black p-4 rounded-xl'>TailWind</h1>
-      <div class = "relative h- [400px] w-[300px] rounded-md">
-        <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" 
-          src="https://images.pexels.com/photos/27334083/pexels-photo-
-          27334083/free-photo-of-a-view-of-a-staircase-with-ornate-carvings.jpeg?auto=
-          compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width="384" height="512"
-        />
-        <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-    <blockquote>
-      <p className="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption class="font-medium">
-      <div class="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
-      </div>
-      <div class="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</div>
+      <Card username="jack" btnText="click me" source="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/02/Batman-89-Comic-Characters-Joker-Jack-Napier-Nicholson.jpg" />
+      <Card username="sara" btnText="click me" source="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"/>
+      <Card username="DK" btnText="click here" source="https://upload.wikimedia.org/wikipedia/commons/b/bb/DK_%282020%29.svg"/>
     </>
   )
 }
