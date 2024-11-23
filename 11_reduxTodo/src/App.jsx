465bb63adde5e1c851606a75ec84a1ae33150d43
Daniyal <daniyalkamran33@gmail.com>
2024-11-23 16:12:38 +0500

    redux

--- 11_reduxTodo/src/App.jsx
+++ 11_reduxTodo/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
   
-
   return (
     <>
-     <AddTodo />
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
